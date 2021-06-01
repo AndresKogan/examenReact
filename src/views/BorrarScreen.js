d5e78a7bdbd44cb9244fc6a9a617a17466d7d88e
Edgardo Gustavo Vergara - UTN <edgardo.vergara@alumnos.frm.utn.edu.ar>
2021-06-01 13:55:16 -0300

    Borrar "soft delete"

--- src/views/BorrarScreen.js
+++ src/views/BorrarScreen.js
@@ -3,9 +3,10 @@ import { useHistory, useParams } from 'react-router';
 import { getMovies } from '../helpers/getMovies';
 
 const BorrarScreen = () => {
-    
-    const {id} = useParams()
+
+    const { id } = useParams()
     const [detalle, setdetalle] = useState(null)
+
     let history = useHistory()
 
     useEffect(() => {
@@ -13,8 +14,24 @@ const BorrarScreen = () => {
             .then(res => setdetalle(res[0]))
     }, [])
 
-     const borrar = null
 
+    const borrar = async () => {
+        const response = await fetch(`http://localhost:3001/movies/update/${id}`, {
+            method: "POST",
+            body: new URLSearchParams({
+                'title': detalle.title + " BORRADO",
+                'rating': detalle.rating,
+                'awards': detalle.awards,
+                'release_date': detalle.release_date,
+                'length': detalle.length,
+                'genre_id': detalle.genre_id
+            })
+        })
+
+        if (!response.ok) {
+            console.log('Error al modificar la pelicula')
+        }
+    }
 
     if (detalle === null) {
         return (<div></div>)
@@ -22,9 +39,9 @@ const BorrarScreen = () => {
 
     return (
         <div>
-        
-        Estas seguro de querer borrar esta película: {`${detalle.title} ???`}
-        <button onClick={borrar}>Borrar</button>
+
+            Estas seguro de querer borrar esta película: {`${detalle.title} ???`}
+            <button onClick={borrar}>Borrar</button>
         </div>
     )
 }
--- src/views/HomeScreen.js
+++ src/views/HomeScreen.js
@@ -3,14 +3,14 @@ import React from 'react'
 
 const HomeScreen = () => {
   return (
-    
-      <div className="row mt-5">
-        <div className="col-4">
-          <legend class="fw-bold">Digital Movies</legend>
-      <p>Bienvenidos a Digital Movies</p>
 
-        </div>
+    <div className="row mt-5">
+      <div className="col-4">
+        <legend class="fw-bold">Digital Movies</legend>
+        <p>Bienvenidos a Digital Movies</p>
+        <a href="/moviesScreen" class="stretched-link">Listado de Películas</a>
       </div>
+    </div>
 
   )
 }
